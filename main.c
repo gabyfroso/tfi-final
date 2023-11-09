203e678cb99b885e0d8667bcefb2c34bb460e00f
Gabyfroso <Gabyfroso@gmail.com>
2023-11-09 08:53:01 -0300

    todo listo

--- main.c
+++ main.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include "utils/methods.h"
+#include "utils/methods.c"
 
-int main() {
+/*
+    Grupo:
+
+    FyN: Frosoni, Hugo Gabriel 1k2
+    Alias: Gabyfroso
+    DNI: 44866295
+    Legajo: 56657
+    Correo: Gabyfroso@gmail.com
+
+*/
+
+void invertir_arreglo(int arr[], int n)
+{
+    int temp;
+    for (int i = 0; i < n / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+}
+
+int main()
+{
     int N_Cantidad;
     printf("Ingrese la cantidad de elementos del arreglo: ");
     scanf("%d", &N_Cantidad);
-
     int arr[N_Cantidad];
-    printf("Ingrese los elementos del arreglo uno por uno:\n");
-    for (int i = 0; i < N_Cantidad; i++) {
+
+
+    printf("Ingrese los elementos del arreglo uno en uno:\n");
+    for (int i = 0; i < N_Cantidad; i++)
+    {
         printf("Elemento %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
 
     printf("\nArreglo antes de ordenar:\n");
-    for (int i = 0; i < N_Cantidad; i++) {
+    for (int i = 0; i < N_Cantidad; i++)
+    {
         printf("%d ", arr[i]);
     }
 
-    int choice;
+indexreturn:
+    int choice, invert;
     printf("\n\nElija el metodo de ordenamiento:\n");
+    printf("0. salir\n");
     printf("1. Burbuja mejorada\n");
     printf("2. Insercion\n");
     printf("3. MergeSort\n");
@@ -28,20 +57,41 @@ int main() {
     printf("Opcion: ");
     scanf("%d", &choice);
 
-    switch (choice) {
-        case 1:
-            bubbleSort(arr, N_Cantidad);
-            break;
-        case 2:
-            intercion(arr, N_Cantidad);
-            break;
-        // Añade más casos según los métodos disponibles
-        default:
-            printf("Opcion invalida\n");
+    do
+    {
+        printf("\n0. Ascendente\n1. Descendente\n");
+        printf("Opcion: ");
+        scanf("%d", &invert);
+    } while (invert != 0 && invert != 1);
+
+    switch (choice)
+    {
+    case 1:
+        bubbleSort(arr, N_Cantidad);
+        break;
+    case 2:
+        intercion(arr, N_Cantidad);
+        break;
+    case 3:
+        MergueSort(arr, 0, N_Cantidad-1);
+        break;
+    case 4:
+        Quicksort(arr, 0, N_Cantidad-1);
+        break;
+    case 5:
+        Seleccion(arr, N_Cantidad);
+        break;
+    default:
+        printf("Opcion invalida\n");
+        goto indexreturn;
     }
 
+    if (invert == 1)
+        invertir_arreglo(arr, N_Cantidad);
+
     printf("\nArreglo despues de ordenar:\n");
-    for (int i = 0; i < N_Cantidad; i++) {
+    for (int i = 0; i < N_Cantidad; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
--- utils/methods.c
+++ utils/methods.c
@@ -1,35 +1,67 @@
 #include "methods.h"
+#include <stdio.h>
+
+/*
+MENU edit cmd
+*/
+
+void indexcmd(int arr[], int n)
+{
+    printf("\x1B[%dm\x1B[%dm", 3, 34);
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("\x1B[0m");
+}
+/*
+FIN MENU edit cmd
+*/
+
 
 // Función de intercambio de dos valores
-void intercambio(int* a, int* b) {
+void intercambio(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int arr[], int n)
+{
     int i, j;
     int swapped;
-    for (i = 0; i < n-1; i++) {
+    for (i = 0; i < n - 1; i++)
+    {
         swapped = 0;
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                intercambio(&arr[j], &arr[j+1]);
+        for (j = 0; j < n - i - 1; j++)
+        {
+            indexcmd(arr, n);//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            if (arr[j] > arr[j + 1])
+            {
+                intercambio(&arr[j], &arr[j + 1]);
                 swapped = 1;
             }
         }
-        if (swapped == 0) {
+        if (swapped == 0)
+        {
             break;
         }
     }
 }
 
 // Implementación del algoritmo de inserción (insertion sort)
-void intercion(int arr[], int n) {
+void intercion(int arr[], int n)
+{
     int i, key, j;
-    for (i = 1; i < n; i++) {
+    for (i = 1; i < n; i++)
+    {
         key = arr[i];
         j = i - 1;
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
+            indexcmd(arr, n);//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             arr[j + 1] = arr[j];
             j = j - 1;
         }
@@ -38,16 +70,9 @@ void intercion(int arr[], int n) {
 }
 
 // Implementación del algoritmo MergeSort
-void MergueSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        MergueSort(arr, l, m);
-        MergueSort(arr, m + 1, r);
-        merge(arr, l, m, r);
-    }
-}
+void merge(int arr[], int l, int m, int r)
 
-void merge(int arr[], int l, int m, int r) {
+{
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
@@ -61,45 +86,57 @@ void merge(int arr[], int l, int m, int r) {
     i = 0;
     j = 0;
     k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
             arr[k] = L[i];
             i++;
-        } else {
+        }
+        else
+        {
             arr[k] = R[j];
             j++;
         }
         k++;
     }
 
-    while (i < n1) {
+    while (i < n1)
+    {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
+    while (j < n2)
+    {
         arr[k] = R[j];
         j++;
         k++;
     }
 }
-
-// Implementación del algoritmo QuickSort
-void Quicksort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        Quicksort(arr, low, pi - 1);
-        Quicksort(arr, pi + 1, high);
+void MergueSort(int arr[], int l, int r)
+{
+    indexcmd(arr, r+1);//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        MergueSort(arr, l, m);
+        MergueSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
-int partition(int arr[], int low, int high) {
+// Implementación del algoritmo QuickSort
+int partition(int arr[], int low, int high)
+{
     int pivot = arr[high];
     int i = (low - 1);
 
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
             i++;
             intercambio(&arr[i], &arr[j]);
         }
@@ -107,14 +144,30 @@ int partition(int arr[], int low, int high) {
     intercambio(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
+void Quicksort(int arr[], int low, int high)
+{
+    indexcmd(arr, high+1); //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        Quicksort(arr, low, pi - 1);
+        Quicksort(arr, pi + 1, high);
+    }
+}
+
 
 // Implementación del algoritmo de selección (selection sort)
-void Seleccion(int arr[], int n) {
+void Seleccion(int arr[], int n)
+{
     int i, j, minIndex;
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < n - 1; i++)
+    {
+        indexcmd(arr, n); //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         minIndex = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
                 minIndex = j;
             }
         }
